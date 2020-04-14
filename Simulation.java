03eef928474d0a10afa216e0913700694d421dd9
Jean-Danniel <35776341+Jean-Danniel@users.noreply.github.com>
2020-04-14 07:16:04 -0400

    Completely changed simulation
    This is the newest version of the simulation. Removed unnecessary such as moods and energy. We are moving towards more of outputting to files statistics such as products sold, sales, etc.

--- Simulation.java
+++ Simulation.java
@@ -1,29 +1,19 @@
 import java.util.*;
 public class Simulation{
     public static void main(String[] args){
-        Personel employee1 = new Personel();
-        
-        //System.out.println(employee1.toString());
-        //Set method tests
-        employee1.setWage(2.0);
-        System.out.println(employee1.getWage()); // It worked, yields 17
+        Personel employee1 = new Personel("Neil DeGrasse",62,101,23.0,6,5);
+        Personel employee2 = new Personel("Veronica Wright",43,102,20.0,7,5);
+        Personel employee3 = new Personel("Jamal Daquivuos",32,103,18.0,7,5);
+        Personel employee4 = new Personel("Bobe Ndimbe",50,104,21.5,6,5);
+        Personel employee5 = new Personel("Julio Bonitez",27,105,17.25,8,5);
+        Personel employee6 = new Personel("Ruth Klin",29,106,19.75,8,5);
+        Personel employee7 = new Personel("Maria Vasqez",41,107,22.0,7,5);
+        Personel employee8 = new Personel("Phillip LeBlanc",45,108,19.0,7,5);
+        Personel employee9 = new Personel("Frank CatFish",36,109,16.5,7,5);
+        Personel employee10 = new Personel("Luisa Ywuvs",22,110,15.0,8,5);
 
-        employee1.setTasks(6);
-        System.out.println(employee1.getTasks()); //It worked, yields 6
-
-        //Creating a collection representing moods of a person
-        Map<Integer, String> moods = new HashMap<Integer, String>();
-        moods.put(0,"Sad");
-        moods.put(1,"Angry");
-        moods.put(2,"Anxious");
-        moods.put(3,"Fearful");
-        moods.put(4,"Neutral");
-        moods.put(5,"Happy");
-        moods.put(6,"Excitted");
-
-        //Testing out the Map collection
-        System.out.println(moods.get(4));
-        
+        Manager mngr1 = new Manager("Elizabeth Meyers",64,201,30.45,6,5);
+        Manager mngr2 = new Manager("Carlos Volvoteres",60,202,30.25,6,5);
 
     }
 }
