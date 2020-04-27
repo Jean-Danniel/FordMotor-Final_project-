bc5fe0731430c33557e69d3084fa6a6a06ec61b9
Jean-Danniel <35776341+Jean-Danniel@users.noreply.github.com>
2020-04-27 09:31:34 -0400

    Updated Simulation class
    This is the newest version

--- Simulation.java
+++ Simulation.java
@@ -14,11 +14,11 @@ public class Simulation{
         Personel employee9 = new Personel("Frank CatFish",36,109,16.5,7,5);
         Personel employee10 = new Personel("Luisa Ywuvs",22,110,15.0,8,5);
 
-        Personel option1 = new Personel();
-        Personel option2 = new Personel();
-        Personel option3 = new Personel();
-        Personel option4 = new Personel();
-        Personel option5 = new Personel();
+        Personel option1 = new Personel("Kevin Francis",44,111,17.35,5,5);
+        Personel option2 = new Personel("DeVanny Tan",23,112,12.45,3,7);
+        Personel option3 = new Personel("Yamila Shukran",34,113,22.50,6,5);
+        Personel option4 = new Personel("Johnson Johnson",59,114,23.0,6,5);
+        Personel option5 = new Personel("Stephanie Newton",36,115,20.0,6,5);
 
         Manager mngr1 = new Manager("Elizabeth Meyers",64,201,30.45,6,5);
         Manager mngr2 = new Manager("Carlos Volvoteres",60,202,30.25,6,5);
@@ -32,17 +32,31 @@ public class Simulation{
         employee1.completeTask(2);
         System.out.println(employee1.getTasks());
 
+        ArrayList<Personel> possible_person = new ArrayList<Personel>();
+        possible_person.add(option1);
+        possible_person.add(option2);
+        possible_person.add(option3);
+        possible_person.add(option4);
+        possible_person.add(option5);
 
-
-        // Personel[] g2 = {employee2,employee4,employee6,employee8,employee10,mngr2};
+        //LISTS of employees in groups
+        ArrayList<Personel>g2 = new ArrayList<Personel>();
+        g2.add(mngr2);
+        g2.add(employee2);
+        g2.add(employee4);
+        g2.add(employee6);
+        g2.add(employee8);
+        g2.add(employee10);
+        
 
         ArrayList<Personel> g1 = new ArrayList<Personel>();
+        g1.add(mngr1);
         g1.add(employee1);
         g1.add(employee3);
         g1.add(employee5);
         g1.add(employee7);
         g1.add(employee9);
-        g1.add(mngr1);
+        
         //To generate random amount of tasks to employees
         int to_do = (int)(Math.random() * 5) + 1;
         
@@ -56,8 +70,37 @@ public class Simulation{
         //         System.out.println(g1[i].getName() + " didn't complete their tasks"); 
         //     }
         // }
+        
 
-        g1 = mngr1.firing(g1,employee1);
+        mngr1.firing(g1,employee1);  //This method works
+        mngr1.displayTeam(g1);  //This method works it displays the team
+        mngr1.hiring(g1, possible_person,option5);
+        mngr1.displayTeam(g1); //This method works
+        
+        System.out.println("\n");
+        Supervisor supervisor = new Supervisor("Johnny Deep",56,301,69.0,3,3);
+        System.out.println(supervisor.getDays()); //Most of the basic method for age,name, etc work
+
+        //Supervisor class methods
+        System.out.println(supervisor.getRaw_goods());
+
+        supervisor.setSales(2000);
+        //System.out.println(supervisor.getSales());
+        supervisor.setFin_goods(5000);
+        supervisor.sell(4999);   //sell method works properly
+        //System.out.println(supervisor.getFin_goods());  Just making sure
+
+        supervisor.setRaw(100);
+        supervisor.buying(25); //buying method works
+        //System.out.println(supervisor.getRaw_goods()); works yields 125 as expected
+
+        //Examples 
+        employee2.setTasks(10);
+        employee2.completeTask(8);
+
+        supervisor.taskToGoods(employee2);  //Method works
+        System.out.println(employee2.getCompTasks());
+
         
     }
 }
