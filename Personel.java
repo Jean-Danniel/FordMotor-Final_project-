bc7e6d7b7807501c6854d3a3446b33464d9f3ea7
Jean-Danniel <35776341+Jean-Danniel@users.noreply.github.com>
2020-05-21 13:01:10 -0400

    Update Personel
    The newest version

--- Personel.java
+++ Personel.java
@@ -1,5 +1,16 @@
-public class Personel{
-    private String name;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package better_final_project;
+
+/**
+ *
+ * @author Jean-daniel
+ */
+public class Personel {
+        private String name;
     private int age;
     private int id;
     private double wage;
